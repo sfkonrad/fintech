55f3da5b5a54725841510b0358d67fd91ca6d2e5
sfkonrad <konrad.kozicki@gmail.com>
2020-12-22 02:12:30 -0800

    "from remote, to local, back to remote"

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 print()
 def main(name):
-<<<<<<< HEAD
-    print(f"Hello, my name is {name}-o.")
-
-main("Konrad") #call <main(name)> function with an "-o"
-=======
     print(f"Hello, my name is {name}-oo.")
 
 main("Konrad") #call <main(name)> function
-#from remote
->>>>>>> c04247950dc97a8e4ce06d45e1193bf5217b2b4a
+print("from remote, to local, back to remote")
